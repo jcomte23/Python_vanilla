e846ac0c3a2e621773545afbaf2173079db19720
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-27 16:34:06 -0500

    carga de operadores logicos

---
+++ 06_Condicionales_con_operadores_logicos/main.py
@@ -0,0 +1,34 @@
+print("")
+print("#"*18)
+print("OPERADORES VISTOS")
+print("#"*18)
+print("relacionales (>, <, >=, <= , ==, !=)")
+print("matemáticos (+, -, *, /, //, **, %)")
+print("")
+
+print("")
+print("#"*18)
+print("OPERADORES LOGICOS")
+print("#"*18)
+print("lógicos (and, or y not)")
+print("Estos operadores se emplean fundamentalmente en las estructuras condicionales para agrupar varias condiciones simples")
+print("")
+
+condicion1=True
+condicion2=True
+
+
+if(condicion1==True and condicion2==True):
+    print("PRIMER BANDERA")
+    
+if(condicion1==True or condicion2==True):
+    print("SEGUNDA BANDERA")
+
+if(not condicion1==False):
+    print("TERCERA BANDERA")
+    
+# EJEMPLOS
+print("validar si un mes esta en el primer trimestre del año")
+mes=input("Mes =>")
+if mes=="enero" or mes=="febrero" or mes=="marzo":
+    print("Corresponde al primer trimestre")
