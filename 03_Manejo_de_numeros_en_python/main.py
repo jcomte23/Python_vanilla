56079e42a158835e9e2687fac38ad7b73ed2010d
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-27 15:41:48 -0500

    mejora en la seccion de numeros

--- 03_Manejo_de_numeros_en_python/main.py
+++ 03_Manejo_de_numeros_en_python/main.py
@@ -1,3 +1,5 @@
+import math
+
 print("*"*41)
 print("|       Manejo de numeros en python      |")
 print("*"*41)
@@ -13,9 +15,24 @@ numeroB = int(input("Ingresa un número entero para 'numeroB': "))
 numeroC = float(input("Ingresa un número decimal para 'numeroC': "))
 
 print()
+print("Conversion de numeros")
+print("Python permite convertir entre diferentes tipos de números, como enteros, flotantes y complejos.")
+print()
+
+entero = int(3.14)
+flotante = float("3.14")
+
+print()
 print(f"Valor de numeroB: {numeroB}, Tipo: {type(numeroB)}")
 print(f"Valor de numeroC: {numeroC}, Tipo: {type(numeroC)}")
 
+# Otras funciones matemáticas
+print(math.sqrt(25))  # Imprime la raíz cuadrada de 25
+print(math.factorial(5))  # Imprime el factorial de 5
+
+# Constantes matemáticas
+print(math.pi)  # Imprime el valor de π
+print(math.e)   # Imprime el valor de e
 
 
 
