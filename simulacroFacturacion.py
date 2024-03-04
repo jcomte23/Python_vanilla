ce555140284200b0584453c67f343f63ff88eaa3
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-03-04 11:56:19 -0500

    creacion de la lista de productos

--- simulacroFacturacion.py
+++ simulacroFacturacion.py
@@ -1,3 +1,16 @@
-
+productos=(
+    {
+        "nombre":"pan",
+        "valor":1000
+    },
+    {
+        "nombre":"galleta",
+        "valor":800
+    },
+    {
+        "nombre":"arina",
+        "valor":1500
+    }
+)
 
 
