4159e57c45647603083e243702cc5dd9cbd5b863
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-03-06 10:07:59 -0500

    avances en el simulacro de facturacion

--- 10_Simulacros/SimulacroFuerte_Facturacion.py
+++ 10_Simulacros/SimulacroFuerte_Facturacion.py
@@ -1,29 +1,29 @@
-productos=(
+productos = (
     {
-        "nombre":"pan",
-        "valor":1000
+        "nombre": "pan",
+        "valor": 1000
     },
     {
-        "nombre":"galleta",
-        "valor":800
+        "nombre": "galleta",
+        "valor": 800
     },
     {
-        "nombre":"arina",
-        "valor":1500
+        "nombre": "arina",
+        "valor": 1500
     }
 )
 
-documentoCliente=""
-nombreCliente=""
-apellidoCliente=""
-factura=[]
+documentoCliente = ""
+nombreCliente = ""
+apellidoCliente = ""
+factura = []
 
 while True:
     print()
     print("########")
     print("--MENU--")
     print("########")
-    menu="""
+    menu = """
     (1) Registar cliente  
     (2) Registrar un nuevo producto a la factura  
     (3) Listar productos actuales de la factura.   
@@ -32,88 +32,93 @@ while True:
     """
     print(menu)
 
-    opcionMenu=input("INGRESE LA OPCION=>")
+    opcionMenu = input("INGRESE LA OPCION=>")
 
-    if opcionMenu=="1":
-        if(documentoCliente=="" or nombreCliente=="" or apellidoCliente==""):
+    if opcionMenu == "1":
+        if (documentoCliente == "" or nombreCliente == "" or apellidoCliente == ""):
             print("-------------------")
             print("Vamos a registrarte")
-            documentoCliente=input("ingrese el # documento=>")
-            nombreCliente=input("ingrese los nombres=>")
-            apellidoCliente=input("ingrese los apellidos=>")
+            documentoCliente = input("ingrese el # documento=>")
+            nombreCliente = input("ingrese los nombres=>")
+            apellidoCliente = input("ingrese los apellidos=>")
             print("-------------------")
             print("Datos registrados")
-            print("DOC:",documentoCliente)
-            print("NOMBRES:",nombreCliente)
-            print("APELLIDOS:",apellidoCliente)
+            print("DOC:", documentoCliente)
+            print("NOMBRES:", nombreCliente)
+            print("APELLIDOS:", apellidoCliente)
         else:
             print("-------------------")
             print("Ya hay un usuario registrado")
-            print("DOC:",documentoCliente)
-            print("NOMBRES:",nombreCliente)
-            print("APELLIDOS:",apellidoCliente)
+            print("DOC:", documentoCliente)
+            print("NOMBRES:", nombreCliente)
+            print("APELLIDOS:", apellidoCliente)
             print("Estas seguro que quieres actualizarlo?")
-            respuesta=input("(1) si (2) no =>")
-            if respuesta=="1":
-                documentoCliente=input("ingrese el # documento=>")
-                nombreCliente=input("ingrese los nombres=>")
-                apellidoCliente=input("ingrese los apellidos=>")
+            respuesta = input("(1) si (2) no =>")
+            if respuesta == "1":
+                documentoCliente = input("ingrese el # documento=>")
+                nombreCliente = input("ingrese los nombres=>")
+                apellidoCliente = input("ingrese los apellidos=>")
                 print("Datos registrados")
-                print("DOC:",documentoCliente)
-                print("NOMBRES:",nombreCliente)
-                print("APELLIDOS:",apellidoCliente)
+                print("DOC:", documentoCliente)
+                print("NOMBRES:", nombreCliente)
+                print("APELLIDOS:", apellidoCliente)
                 print("Usuario actualizado")
-    elif opcionMenu=="2":
-        indice=0
+    elif opcionMenu == "2":
+        # aca listamos los productos disponibles
+        indice = 0
         for producto in productos:
             print(f"[{indice}] {producto["nombre"]} {producto["valor"]}")
-            indice+=1
-        productoSeleccionado=int(input("ingrese el indice del producto que quieres agregar?=>"))
-        cantidad=int(input("cuantas unidades vas a comprar?=>"))
-        totalProducto=cantidad*productos[productoSeleccionado]["valor"]
-        nuevoProductoEnFactura={
-            "nombre":productos[productoSeleccionado]["nombre"],
-            "valor":productos[productoSeleccionado]["valor"],
-            "unidades":cantidad,
-            "totalProducto":totalProducto
+            indice += 1
+        # en esta parte le pedimos al usuario que nos diga cual producto va agregar a la factura
+        productoSeleccionado = int(input("ingrese el indice del producto que quieres agregar?=>"))
+        cantidad = int(input("cuantas unidades vas a comprar?=>"))
+        totalProducto = cantidad * productos[productoSeleccionado]["valor"]
+
+        # creamos un nuevo diccionario para guardarlo en la factura
+        nuevoProductoEnFactura = {
+            "nombre": productos[productoSeleccionado]["nombre"],
+            "valor": productos[productoSeleccionado]["valor"],
+            "unidades": cantidad,
+            "totalProducto": totalProducto
         }
         factura.append(nuevoProductoEnFactura)
-    elif opcionMenu=="3":
-        cantidadProductos=len(factura)
-        valorNeto=0
+        print("producto agregado a la factura")
+    elif opcionMenu == "3":
+        valorTotalFactura = 0
         for producto in factura:
-            valorNeto+=producto["totalProducto"]
-        impuestoIva=valorNeto*0.19
-        totalFactura=valorNeto+impuestoIva
+            valorTotalFactura += producto["totalProducto"]
+
+        print("FACTURA")
+        print("Datos registrados")
+        print("DOC:", documentoCliente)
+        print("NOMBRES:", nombreCliente)
+        print("APELLIDOS:", apellidoCliente)
+        for producto in factura:
+            print(f"{producto["nombre"]} {producto["valor"]}")
+
+
+        print("--------------------")
+        print(valorTotalFactura)
+
+        cantidadProductos = len(factura)
+        valorNeto = 0
+        for producto in factura:
+            valorNeto += producto["totalProducto"]
+        impuestoIva = valorNeto * 0.19
+        totalFactura = valorNeto + impuestoIva
         print("--------------------")
         print("productos en factura")
         print("--------------------")
         for producto in factura:
             print(f"{producto["nombre"]} {producto["valor"]} * {producto["unidades"]} = {producto["totalProducto"]}")
         print("--------------------")
-    elif opcionMenu=="4":
-        
-        valorTotalFactura=0
-        
-        for producto in factura:
-            valorTotalFactura+=producto["totalProducto"]
-        print("--------------------")
-        print(valorTotalFactura)
-        
-        
-        print("FACTURA")
-        print("Datos registrados")
-        print("DOC:",documentoCliente)
-        print("NOMBRES:",nombreCliente)
-        print("APELLIDOS:",apellidoCliente)
-        for producto in factura:
-            print(f"{producto["nombre"]} {producto["valor"]}")
-    elif opcionMenu=="5":
+    elif opcionMenu == "4":
+
+
+    elif opcionMenu == "5":
         print("Feliz dia")
         break
     else:
         print("#############################")
         print("ingreste una opcion invalida")
         print("#############################")
-
-
