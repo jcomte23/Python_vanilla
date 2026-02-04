b47810777454231eba69855fc51e404291192d22
Javier Combita Tellez <javiercombita2014@gmail.com>
2026-02-04 11:40:14 -0500

    feat: se creo un ejercicio de practica

---
+++ 10_Simulacros/SimulacroBasico_AnalizadorDeTexto.py
@@ -0,0 +1,20 @@
+texto=input("ingresa un texto cualquiera=>").lower()
+letras=input("ingresa 3 letras a tu elección=>").lower()
+
+print(f"la letra {letras[0]} aparece {texto.count(letras[0])} veces")
+print(f"la letra {letras[1]} aparece {texto.count(letras[1])} veces")
+print(f"la letra {letras[2]} aparece {texto.count(letras[2])} veces")
+
+cantidad_de_palabras=texto.split()
+print(f"en el el texto hay {len(cantidad_de_palabras)} palabras")
+
+primer_letra=texto[0]
+ultima_letra=texto[-1]
+print(f"la primer letra es: {primer_letra}")
+print(f"la ultima letra es: {ultima_letra}")
+print(f"el texto invertido es: {cantidad_de_palabras[::-1]}")
+
+if "python" in texto:
+    print(f"python esta en el texto")
+else:
+    print(f"python no esta en el texto")
